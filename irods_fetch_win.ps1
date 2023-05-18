e16900b16acafb940f3e660bc5ced73123ebe007
Radek <radek.veverka@icloud.com>
2023-05-18 18:50:50 +0200

    dev

--- irods_fetch_win.ps1
+++ irods_fetch_win.ps1
@@ -1,1 +1,3 @@
-# exec url: "ffff"
+# exec url: Invoke-Command -ScriptBlock ([Scriptblock]::Create((iwr "https://raw.githubusercontent.com/cemcof/cemcof.github.io/main/irods_fetch_win.ps1").Content)) -ArgumentList ($args + @('someargument'))
+Write-Host "Welcome to irods_win_fetcher.ps1"
+Write-Host $args
