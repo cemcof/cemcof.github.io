050fb14b86343929f9e640a683dd9f57bc1d5e98
Radek <radek.veverka@icloud.com>
2024-02-08 10:35:32 +0100

    Dev win script

--- irods_fetch_win.ps1
+++ irods_fetch_win.ps1
@@ -2,32 +2,28 @@
 
 Write-Host "Welcome to irods_win_fetcher.ps1"
 Write-Host $args
+exit 
 
 # Ensure python is installed and available
-if (-not (Test-Path (Get-Command python3 -ErrorAction SilentlyContinue))) {
-    Write-Host "Python is not installed. Installing Python..."
-    # Python is not available, install it via chocolatey...
-    # Check if Chocolatey is installed
-    if (-not (Test-Path (Get-Command choco -ErrorAction SilentlyContinue))) {
-        Write-Host "Chocolatey is necessary for installing python. Installing Chocolatey..."
-        Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-        Write-Host "Chocolatey has been succesfully installed."
-    } 
-
-    choco install python -y
-    Write-Host "Python has been successfully installed."
+if (-not (Get-Command python3 -ErrorAction SilentlyContinue)) {
+    Write-Host "Python is not installed. Please install python3 on your computer."
+    exit
 } 
 
 # Ensure python-irodsclient is installed and available
-if (-not (python3 -c "import irods" 2>&1)) {
-    Write-Host "irods module is not installed. Installing irods..."
-    python3 -m pip install irods
-    Write-Host "`e[32mirods module has been successfully installed.`e[0m"
+python3 -c "import irods" 2>$null
+
+if (-not ($?)) {
+    Write-Host "irods python module is not installed. Installing irods..."
+    python3 -m pip install python-irodsclient
+    Write-Host "irods module has been successfully installed."
 }
 
 # Download and invoke the downloader script 
 $scriptUrl = "https://raw.githubusercontent.com/cemcof/cemcof.github.io/main/irods_fetch.py"
 $pythonScript = (Invoke-WebRequest -Uri $scriptUrl).Content
+# Write-Host $pythonScript
+exit
 $pythonScript | python3 - 
 
 
